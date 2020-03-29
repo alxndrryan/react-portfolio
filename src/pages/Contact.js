046be646b6f781e7c1a0837564416192c65fdab6
Rand al'Thor <alexanderhogle@gmail.com>
2020-03-28 20:19:13 -0500

    not sure

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -11,6 +11,45 @@ function Contact() {
           <hr />
         </Col>
       </Row>
+      <Row>
+        <Col size="md-1"></Col>
+        <Col size="sm-6">
+          <form>
+            <div className="form-group">
+              <label for="exampleInputEmail1">Name</label>
+              <input
+                type="text"
+                className="form-control"
+                placeholder=""
+                id="exampleInputEmail1"
+                aria-describedby="emailHelp"
+                placeholder="Enter email"
+              />
+              <div className="form-group">
+                <label for="exampleInputPassword1">Email</label>
+                <input
+                  type="email"
+                  className="form-control"
+                  placeholder=""
+                  id="exampleInputPassword1"
+                  placeholder="Password"
+                />
+                <div className="form-group">
+                  <label for="exampleFormControlTextarea1">Message</label>
+                  <textarea
+                    className="form-control"
+                    id="exampleFormControlTextarea1"
+                    rows="3"
+                  ></textarea>
+                </div>
+                <button type="submit" className="btn btn-primary button">
+                  Submit
+                </button>
+              </div>
+            </div>
+          </form>
+        </Col>
+      </Row>
     </Container>
   );
 }
